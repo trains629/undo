f3a40910afa920a4c8157040c760e2a11341f9c2
trains629 <trains629@foxmail.com>
2020-02-28 22:17:13 +0800

    增加测试脚本

---
+++ undo.test.js
@@ -0,0 +1,33 @@
+import Undo from "./index"
+
+const undo = new Undo(10)
+
+test('insert 5 items ', () => {
+  let count = 5;
+  for (let index = 0; index < count; index++) {
+    undo.insert(index)
+  }
+  expect(undo.getValue()).toBe(count -1)
+})
+
+test('undo() 2 insert() length to 4', () => {
+  let id = 5;
+  undo.undo()
+  undo.undo()
+  undo.insert(id);  
+  expect(undo.getLength()).toBe(4)
+})
+
+test('insert 7', () => {
+  let count = 7;
+  let old = undo.getLength()
+  for (let index = 0; index < count; index++) {
+    undo.insert(index);
+  }
+  expect(undo.getLength()).toBe(old + count - 1);
+  expect(undo.getValue(0)).toBe(1);
+  expect(undo.getValue(old)).toBe(1);
+})
+
+
+
